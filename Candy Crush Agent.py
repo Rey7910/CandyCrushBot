7e694b776066f037e3f2428e036c1a50cff61ec2
Rey7910 <rtoledoleguizamon@gmail.com>
2023-08-27 20:33:12 -0500

    click and shoot ss

--- Candy Crush Agent.py
+++ Candy Crush Agent.py
@@ -1,16 +1,43 @@
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+import pyautogui
+import time
+from PIL import ImageGrab
 
 
-url = "https://www.cooljuegos.com/juego-en-linea/candy-crush/"
+def Click():
+    # Wait a period of time before continue with the rest of code
+    time.sleep(5)
 
-driver = webdriver.Chrome()
-driver.get(url)
+    # Click cordenates
+    x_coord = 500
+    y_coord = 500
+
+    # Move the mouse and click
+    pyautogui.moveTo(x_coord, y_coord)
+    pyautogui.click()
 
 
-driver.maximize_window()
+def shoot_ss():
+
+    # Define limits per each side of the screen (izquierda, superior, derecha, inferior)
+    bounding_box = (100, 100, 500, 500)
+
+    # Capture the specific region throughout the screen
+    screenshot = ImageGrab.grab(bbox=bounding_box)
+
+    # Resize the image to a lower resolution
+    #resized_screenshot = screenshot.resize((320, 240))
+
+    # Save the image
+    #resized_screenshot.save("captura_baja_resolucion.png")
+
+def pyautogui_ss():
+        # Tomar una captura de pantalla completa
+    screenshot = pyautogui.screenshot()
+
+    # Guardar la captura de pantalla en un archivo
+    screenshot.save("captura_de_pantalla.png")
 
 
-input("In order to shut down the browser press enter...")
 
-driver.quit() 
+pyautogui_ss()
+Click()
