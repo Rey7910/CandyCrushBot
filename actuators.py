236d57e0a2252ba02099c01bf8168f03a7b620c6
Rey7910 <rtoledoleguizamon@gmail.com>
2023-08-29 22:41:24 -0500

    Actuators finished for full-screen (Rey-PC)

--- actuators.py
+++ actuators.py
@@ -1,12 +1,17 @@
 import pyautogui
 import time
 
-top=80
-left=130
+# Acer Nitro 5 (Full Screen) Configuration - Rey
 
-def Move_candy(start_x, start_y,end_x,end_y):
+x_growth_factor = 90
+y_growth_factor = 80
+x_reference = 170
+y_reference = 110
 
-    time.sleep(5)
+
+def move_candy(start_x, start_y,end_x,end_y):
+
+    time.sleep(3)
 
     # Move the mouse to the starting position
     pyautogui.moveTo(start_x, start_y)
@@ -20,7 +25,45 @@ def Move_candy(start_x, start_y,end_x,end_y):
     # Release the mouse button to complete the drag action
     pyautogui.mouseUp()
 
+#90*90
 
-while(True):
-    Move_candy(150,90,250,90)
-    Move_candy(150,90,170,190)
+
+def crash_candy(x,y,direction):
+
+    # Pixel Direction of the movement to the right side
+    if(direction=='right'):
+        move_candy(x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor),x_reference+(x*x_growth_factor)+x_growth_factor,y_reference+(y*y_growth_factor))
+    
+    # Pixel Direction of the movement to the left side
+    elif(direction=='left'):
+        move_candy(x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor),x_reference+(x*x_growth_factor)-x_growth_factor,y_reference+(y*y_growth_factor))
+    
+    # Pixel Direction of the movement to the up side
+    elif(direction=='up'):
+        move_candy(x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor),x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor)-y_growth_factor)
+    
+    # Pixel Direction of the movement to the down side
+    elif(direction == 'down'):
+        move_candy(x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor),x_reference+(x*x_growth_factor),y_reference+(y*y_growth_factor)+y_growth_factor)
+
+
+
+def test_actuators(x,y):
+
+    crash_candy(x,y,'down')
+    crash_candy(x,y,'up')
+    crash_candy(x,y,'left')
+    crash_candy(x,y,'right')
+
+
+def test_full_movements():
+
+    for x in range(9):
+        for y in range(9):
+            crash_candy(x,y,'down')
+            crash_candy(x,y,'up')
+            crash_candy(x,y,'left')
+            crash_candy(x,y,'right')
+
+
+test_full_movements()
