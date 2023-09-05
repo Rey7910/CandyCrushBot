2aaa7dc1b16ff532347a6daa9d52202747f5e8ed
Carlos Rios <carlosandresrio33@hotmail.com>
2023-09-04 20:26:01 -0500

    Candies representations added

--- board_solver/BoardSolver.py
+++ board_solver/BoardSolver.py
@@ -1,3 +1,20 @@
+ORANGE = 0
+BLUE = 1
+RED = 2
+PURPLE = 3
+
+LINE_ORANGE = 10
+LINE_BLUE = 11
+LINE_RED = 12
+LINE_PURPLE = 13
+
+BOOM_ORANGE = 20
+BOOM_BLUE = 21
+BOOM_RED = 22
+BOOM_PURPLE = 23
+
+CHOCO = 66
+
 class BoardSolver:
     def __init__(self) -> None:
         pass
