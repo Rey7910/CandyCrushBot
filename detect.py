ef4037ea3ca755ce51469936c519f8683ad0bf24
Rey7910 <rtoledoleguizamon@gmail.com>
2023-09-21 11:05:12 -0500

    horizontal lines from green candy added

--- detect.py
+++ detect.py
@@ -36,18 +36,18 @@ def identifyCandy(r,g,b):
     elif( (r in range(1200,1276) and g in range(1000,1090) and b in range(130,200)) or (r in range(1200,1276) and g in range(1000,1200) and b in range(380,500))):
         return 15 # "special yellow"
     
-    elif( r in range(780,850) and g in range(1100,1276) and b in range(550,650)):
+    elif( (r in range(780,850) and g in range(1100,1276) and b in range(550,650)) or (r in range(380,500) and g in range(1100,1276) and b in range(180,280))):
         return 14 # "special green"
     
     elif( (r in range(1200,1276) and g in range(650,750) and b in range(600,700)) or (r in range(1100,1200) and g in range(300,420) and b in range(300,420))):
         return 12 # "special red"
 
-    elif( r in range(400,700) and g in range(940,1200) and b in range(1150,1260)):
+    elif( r in range(400,830) and g in range(940,1200) and b in range(1130,1260)):
 
         return 11 # "special blue"   
     
-    elif( (r in range(400,700) and g in range(800,1100) and b in range(1000,1276)) or (r in range(1200,1250) and g in range(800,1100) and b in range(200,700))):
-        #print(r,g,b)
+    elif( (r in range(400,700) and g in range(800,1100) and b in range(1000,1276)) or (r in range(1200,1270) and g in range(800,1100) and b in range(200,700))):
+        print(r,g,b)
         return 10 #"special orange"
     
     elif( r in range(900,1200) and g in range(90,290) and b in range(1270,1276)):
